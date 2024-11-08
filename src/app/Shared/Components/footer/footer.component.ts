270a6d5a118b745c3d95afa136bc952fcdda9d8a
choudharymahipal <mschoudhary81@gmail.com>
2024-11-08 11:05:05 +0530

    footer added

--- src/app/Shared/Components/footer/footer.component.ts
+++ src/app/Shared/Components/footer/footer.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mahi-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  isPlaying: boolean = false;
+  isValume: boolean = true;
 
+  playPauseButton() {
+    this.isPlaying = !this.isPlaying;
+  }
+
+  volumeClicked() {
+    this.isValume = !this.isValume;
+  }
 }
